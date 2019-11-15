f7a8c8f0f8362191909075e29ba50094b09bf4f6
Damian Skrodzki <sn4kerr@gmail.com>
2019-11-15 17:56:45 +0100

    wrzucam

---
+++ UGnaukaC/sumy_100.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int N = 100;
+    int exponent = 10;
+
+    double S_4 = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        S_4 = S_4 + pow(i, exponent);
+       // printf("%.0lf ", pow(i, 4)); 
+       //pow działa tylko no double więc oszukane %.0lf 
+    }
+    printf("\n");
+
+    printf("%.0f\n", S_4); // zrobić tabelkę na 10 wierszy sumy 1 ^1 + 2^1
+    // 1^2+2^2 ....
+}
