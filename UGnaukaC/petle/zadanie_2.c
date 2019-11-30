57d8414b4b679d8384efeacddc5088454103d5fb
Damian Skrodzki <sn4kerr@gmail.com>
2019-11-30 11:46:54 +0100

    petle

--- UGnaukaC/petle/zadanie_2.c
+++ UGnaukaC/petle/zadanie_2.c
@@ -1,7 +1,7 @@
 // Napisz program wyświetlający szachownicę
 #include <stdio.h>
 
-int main()
+/*int main()
 {
 printf("* * * * * * * *\n");
 printf(" * * * * * * * *\n");
@@ -11,4 +11,27 @@ printf("* * * * * * * *\n");
 printf(" * * * * * * * *\n");
 printf("* * * * * * * *\n");
 printf(" * * * * * * * *\n");
+}*/
+
+void print_row_1()
+{
+    printf("* * * * * * * *\n");
+}
+
+void print_row_2()
+{
+    printf(" * * * * * * * *\n");
+}
+
+void_print_x()
+{
+    print_row_1();
+    print_row_2();
+}
+
+int main()
+int rows = 4;
+for(int i = 1; i <= rows; i++)
+{
+    printf_layer();
 }
---
+++ UGnaukaC/petle/zadanie_4.c
@@ -0,0 +1,13 @@
+/* 4. Napisz program, który wyliczy sumę liczb naturalnych od 1 do 
+liczby n podanej przez użytkownika. Kiedy użytkownik poda liczbę 
+mniejszą niż 1, to program powinien wypisać stosowny komunikat o błędzie. */
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    int n = 0;
+    printf("Podaj liczbę naturalną n:\n");
+    scnaf("%d\n", &n);
+}
