ad5d20a6e99c8f7b5482a94484eeffc6539662e7
Damian Skrodzki <sn4kerr@gmail.com>
2019-12-14 11:20:33 +0100

    collatz

--- UGnaukaC/Collatz/zadanie_1.c
+++ UGnaukaC/Collatz/zadanie_1.c
@@ -1,24 +1,22 @@
+/* Napisać program który wypisuje tabelke z problemem collatza, gdzie w pierwszej kolumnie jest liczba od 1 do 100 w drugiej kolumnie opis, że się zgadza
+w trzeciej kolumnie liczba kroków jaką trzeba było wykonac aby otrzymać jeden, w czwartej kolumnie wypisać MAX */
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-	int n, f, cnt = 0;
-
-	printf("Wpisz liczbę: \n");
-	scanf("%d", &n);
+  int n, f, cnt = 0;
+  printf("Wpisz liczbę: \n");
+  scanf("%d", &n);
 	
 	while(n>1)
 	{
-	
 	if(n%2 == 0)
-		f = n/2;
+	 f = n/2;
 	else
-		f = 3*n+1;
-
+	 f = 3*n+1;
 	n = f;
 	printf("\n%d " ,n);
 	cnt++;
 	}
-
 	printf("Trzeba powtórzyć funkcję %d razy, aby otrzymać 1\n\t",cnt);
 }
