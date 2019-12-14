dcb48b8499527c697bdcc0a4f9c3ca58d4e09ead
Damian Skrodzki <sn4kerr@gmail.com>
2019-12-14 10:29:19 +0100

    Collatzs

---
+++ UGnaukaC/Jbernoulli/jbernoulli.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include "jbernoulli.h"
+
+
+
+//TODO
+static unsigned long long int powerll(unsigned long long int x, unsigned  long long int y) 
+{
+    return powl(x, y);
+}
+
+// exported
+unsigned long long int calculate_sum(int y, int upper_limit) 
+{
+    unsigned long long int result = 0;
+
+    for(int i = 1; i <= upper_limit; i++) 
+    {
+        result += powerll(i, y);
+    }
+    return result;
+}
+
+void print_results(int y, int s, int upper_limit) 
+{
+    printf("1^%d + 2^%d + ... + %d^%d = %32d\n", y, y, upper_limit, y, s);
+}
+// long double powl(long double x, long double y);
+// 1. Napisać nową funkcję 'powerll' tak aby liczyła porpawnie.
+// 2. Napisać funkcję wypisującą liczby z podkreśleniami.
+// 3. Skasować zbędne spacje po znaku = w tabelce
