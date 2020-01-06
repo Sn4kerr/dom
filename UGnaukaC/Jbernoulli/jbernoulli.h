2c2dfaa4dd1725ad29616502073c204a412f1d27
Damian Skrodzki <sn4kerr@gmail.com>
2020-01-06 18:42:24 +0100

    Revert "Collatzs"
    This reverts commit dcb48b8499527c697bdcc0a4f9c3ca58d4e09ead.

--- UGnaukaC/Jbernoulli/jbernoulli.h
+++
@@ -1,5 +0,0 @@
-// Calculates sum 1^y+2^y+...+1000^y = s
-unsigned long long int calculate_sum(int y, int upper_limt);
-
-// Wypisuje(prints) 1^y+2^y+...+1000^y = s
-void print_results(int y, int s, int upper_limit);
